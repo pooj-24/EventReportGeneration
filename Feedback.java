47228e621911effdc1cddc236bf48b2855f7c9ce
pooj-24 <pooja.m2023ai-ds@sece.ac.in>
2024-11-14 09:38:11 +0530

    Feedback.java

---
+++ Feedback.java
@@ -0,0 +1,15 @@
+package com.eventreport;
+
+public class Feedback {
+    private String feedbackText;
+
+    
+    public Feedback(String feedbackText) {
+        this.feedbackText = feedbackText;
+    }
+
+
+    public String getFeedbackText() {
+        return feedbackText;
+    }
+}
